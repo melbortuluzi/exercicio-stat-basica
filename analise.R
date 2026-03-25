c39e85db09a0d6b648b75d191f0b871eb4e65150
Melyssa Bortuluzi <bortuluzimelyssa@gmail.com>
2026-03-25 20:46:58 -0300

    analise R atualizada com outras linhas

--- analise.R
+++ analise.R
@@ -1,1 +1,16 @@
 m <- c(1, 2, 3, 4, 5)
+
+# Outras Linhas
+
+  # Calculando a média
+  media <- mean(m)
+  
+  # Mostrando o resultado
+  print(media)
+  
+  # Criando um gráfico simples
+  plot(m,
+       type = "b",
+       main = "Valores do vetor m",
+       xlab = "Índice",
+       ylab = "Valor")
